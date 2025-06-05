43d0138715696e045bf5b03a6f4beb62a4019077
loicmaes <production@maesloic.fr>
2025-06-05 22:10:48 +0200

    :rocket: Setup top bar component (missing responsive)

---
+++ components/shared/TopBar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const links: {
+  label: string;
+  path: string;
+}[] = [
+  {
+    label: "portfolio",
+    path: "/portfolio",
+  },
+  {
+    label: "blog",
+    path: "/blog",
+  },
+  {
+    label: "services",
+    path: "/services",
+  },
+];
+</script>
+
+<template>
+  <header class="sticky top-0 bg-background h-16 flex items-center justify-center px-4">
+    <div class="w-full max-w-7xl flex items-center justify-between gap-12">
+      <NuxtLinkLocale
+        to="/"
+        class="text-lg font-bold"
+      >
+        {{ $t("name") }}.
+      </NuxtLinkLocale>
+
+      <nav class="flex items-center gap-1">
+        <Button
+          v-for="link in links"
+          :key="link.label"
+          variant="ghost"
+          as-child
+        >
+          <NuxtLinkLocale
+            :to="link.path"
+            active-class="!bg-secondary !text-secodary-foreground"
+          >
+            {{ $t(`navigation.${link.label}`) }}
+          </NuxtLinkLocale>
+        </Button>
+        <Button
+          class="ml-7"
+          as-child
+        >
+          <NuxtLinkLocale to="/contact">
+            {{ $t("navigation.lets-meet") }}
+          </NuxtLinkLocale>
+        </Button>
+      </nav>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import TopBar from "~/components/shared/TopBar.vue";
 </script>
 
 <template>
   <div data-layout="default">
-    <NuxtPage />
+    <TopBar />
+
+    <div class="flex justify-center px-4">
+      <div class="w-full max-w-7xl">
+        <NuxtPage />
+      </div>
+    </div>
   </div>
 </template>
