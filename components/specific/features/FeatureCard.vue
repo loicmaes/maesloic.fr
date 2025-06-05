bdd05c28f12786152daa04ed26b4a2af069036cb
loicmaes <production@maesloic.fr>
2025-06-05 23:19:10 +0200

    :rocket: Add features components

---
+++ components/specific/features/FeatureCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { LucideIcon } from "lucide-vue-next";
+
+defineProps<{
+  feature: {
+    icon?: LucideIcon;
+    name: string;
+  };
+}>();
+</script>
+
+<template>
+  <Card
+    :id="feature.name"
+    class="p-6"
+  >
+    <div class="flex">
+      <component
+        :is="feature.icon"
+        v-if="feature.icon"
+        class="size-6"
+      />
+      <CardHeader class="w-full">
+        <CardTitle>{{ $t(`home.what-i-do.features.${feature.name}.title`) }}</CardTitle>
+        <CardDescription>{{ $t(`home.what-i-do.features.${feature.name}.caption`) }}</CardDescription>
+      </CardHeader>
+    </div>
+  </Card>
+</template>
---
+++ components/specific/features/FeatureGrid.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Code, Globe, type LucideIcon, Rocket, Users } from "lucide-vue-next";
+import FeatureCard from "~/components/specific/features/FeatureCard.vue";
+
+withDefaults(defineProps<{
+  features?: {
+    icon?: LucideIcon;
+    name: string;
+  };
+}>(), {
+  features: [
+    {
+      icon: Globe,
+      name: "web-sites",
+    },
+    {
+      icon: Rocket,
+      name: "pwa",
+    },
+    {
+      icon: Users,
+      name: "user-based",
+    },
+    {
+      icon: Code,
+      name: "pretty-code",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="grid sm:grid-cols-2 xl:grid-cols-4 gap-4">
+    <FeatureCard
+      v-for="feature in features"
+      :key="feature.name"
+      :feature="feature"
+    />
+  </div>
+</template>
