0cd2ea84c9f82422e9d90db329f01be13c1bcdbb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-06-05 19:10:04 +0200

    :wrench: Add default modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxt/eslint',
+    'shadcn-nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/color-mode',
+    '@nuxt/image',
+    '@pinia/nuxt'
+  ],
+  i18n: {
+    locales: [
+      {
+
+      }
+    ],
+    defaultLocale: "",
+    strategy: "prefix_except_default",
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {
+        ital: "100..900",
+        wght: "100..900",
+      }
+    }
+  },
+  colorMode: {
+    preference: "system",
+    fallback: "dark"
+    classPrefix: "",
+    classSuffix: "",
+  },
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+});
